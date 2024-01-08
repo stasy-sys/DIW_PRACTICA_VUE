ea58437507c3cfef816f5a29febee53dcb98743f
David Martínez Álvarez <a22davidma@ATEIS.LOCAL>
2024-01-08 09:37:02 +0100

    vue_base

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,16 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: black;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
 }
 </style>
