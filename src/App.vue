708714a5d7e2d7389b450437996a8d1d64c824c5
David Martínez Álvarez <a22davidma@ateis.local>
2024-01-15 09:32:59 +0100

    Clase 15-01-24

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <PrimerasPruebas mensaje="Primeras Pruebas"/>
-  <ListaTareas/>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PrimerasPruebas from './components/PrimerasPruebas.vue'
-import ListaTareas from './components/ListaTareas.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    PrimerasPruebas,
-    ListaTareas
+
   }
 }
 </script>
@@ -22,14 +14,5 @@ export default {
 <style>
 body {
   background-color: black;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #ffffff;
-  margin-top: 60px;
 }
 </style>
